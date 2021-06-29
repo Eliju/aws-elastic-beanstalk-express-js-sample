f40a28171008ad475bdfc1acbd27fc6b20fab8bc
Eliju <36505898+Eliju@users.noreply.github.com>
2021-06-29 17:58:45 +0200

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello You!'));
+app.get('/', (req, res) => res.send('Now this needs to be manually approved!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
